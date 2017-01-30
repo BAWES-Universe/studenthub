e8f1aac70bfbc8da12b063ea1004d0adcad92ba8
Khalid <khalid@bawes.net>
2017-01-30 10:46:29 +0300

    Setup environment db configs

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=payroll',
+            'username' => 'payrollUser',
+            'password' => 'pay',
             'charset' => 'utf8',
         ],
         'mailer' => [
