1629762798e8279c21dcc7a3e4183a4145708516
Khalid <khalid@bawes.net>
2017-02-08 18:56:59 +0300

    Setup mailer to use sendgrid on prod env

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -11,6 +11,14 @@ return [
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.sendgrid.net',
+                'username' => 'sh-payroll',
+                'password' => 'WeLoveSHTrainingProg!121',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
         ],
     ],
 ];
