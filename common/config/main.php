ff1c212c6e06fbf7f2efedbc9b5b3bd5f446c962
Khalid <khalid@bawes.net>
2017-02-08 18:07:39 +0300

    Update common config and dependencies

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,33 @@
 <?php
 return [
+    'name' => 'Payroll',
+    'timeZone' => 'Asia/Kuwait',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'formatter' => [
+            'currencyCode' => 'KWD',
+            'defaultTimeZone' => 'Asia/Kuwait',
+        ],
+        'slack' => [
+            'class' => 'understeam\slack\Client',
+            'url' => 'https://hooks.slack.com/services/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',
+            'username' => 'Payroll',
+        ],
+        'httpclient' => [
+            'class' =>'yii\httpclient\Client',
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'common\components\SlackLogger',
+                    'logVars' => [],
+                    'levels' => ['info', 'error', 'warning'],
+                    'categories' => ['backend\*', 'agent\*', 'common\*', 'console\*', 'api\*'],
+                ],
+            ],
         ],
     ],
 ];
