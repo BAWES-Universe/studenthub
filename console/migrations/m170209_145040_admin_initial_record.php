5c2057949ba6b63e1d00558fd33ac7b1b1055be1
Khalid <khalid@bawes.net>
2017-02-09 18:03:46 +0300

    Setup initial Admin record for dev use

---
+++ console/migrations/m170209_145040_admin_initial_record.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+use yii\db\Expression;
+
+class m170209_145040_admin_initial_record extends Migration
+{
+    public function up()
+    {
+        // Insert the initial Admin record
+        // Email: admin@admin.com
+        // Password: admin
+        $this->insert('admin', [
+            'admin_id' => 1,
+            'admin_name' => "Admin",
+            'admin_email' => "admin@admin.com",
+            'admin_auth_key' => "8ScKbzxR2gIgMd2Eeol9-an6AAHUZztH",
+            'admin_password_hash' => '$2y$13$x7v9oeSViFk3vibhKevkJOTBO603fWR2WTw5.JzYAcddgGj/3ExVG',
+            'admin_password_reset_token' => null,
+            'admin_status' => 10,
+            'admin_created_at' => new Expression("NOW()"),
+            'admin_updated_at' => new Expression("NOW()")
+        ]);
+    }
+
+    public function down()
+    {
+        // Empty the Admin Table
+        $this->execute("SET foreign_key_checks = 0;");
+        $this->truncateTable('admin');
+        $this->execute("SET foreign_key_checks = 1;");
+    }
+}
