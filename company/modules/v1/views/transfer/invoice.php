326dcc52e29bd94d8edae21f356d1a4cc24a0aae
Anil Kumar <anilkumar.dhiman1@gmail.com>
2017-05-16 18:23:16 +0530

    invoice templating done

--- company/modules/v1/views/transfer/invoice.php
+++ company/modules/v1/views/transfer/invoice.php
@@ -1,35 +1,91 @@
-
-<h1>Invoice</h1>
-
-<hr />
-
-For 
-
-<br />
-
-<h3><?= Yii::$app->user->identity->company_name ?></h3>
-
-<br />
-
-<table class="table table-bordered">
-	<thead>
-		<tr>
-			<th>Candidate</th>
-			<th>Hours</th>
-			<th>Bonus</th>
-			<th>Total</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php foreach ($transfer['candidates'] as $key => $value) { ?>
-		<tr>	
-			<td><?= $value['candidate_name'] ?></td>
-			<td><?= $value['hours'] ?></td>
-			<td><?= $value['bonus'] ?></td>
-			<td><?= ($value['hours'] *  $value['company_hourly_rate']) + $value['bonus'] ?></td>
-		</tr>
-		<?php } ?>
-	</tbody>
-</table>
-
-<h4>Invoice Total : <?= $transfer['company_total'] ?></h4>
+<?php
+$totalHours = 0;
+$totalBonus = 0;
+$totalAmount = 0;
+foreach ($transfer['candidates'] as $key => $value) {
+    $totalHours += $value['hours'];
+    $totalBonus += $value['bonus'];
+    $totalAmount += ($value['hours'] * $value['company_hourly_rate']);
+}
+?>
+<div class="row">
+    <div class="col-sm-12" style="margin-top:30px; text-align:center;">
+        <img src="/images/bawes.jpg" style="width:100px; margin-bottom:0;">
+        <h1>Invoice</h1>
+        <hr>
+    </div>
+</div>
+<div class="row" style="padding: 1.85714286em;">
+    <table class="table" cellpadding="3" cellspacing="3">
+        <tr>
+            <td style="width: 56%;">
+                <table cellpadding="2">
+                    <tr><td><h3>Bill to<br></h3></td></tr>
+                    <tr><td><?= $transfer['company']['company_name'] ?></td></tr>
+                    <tr><td><?= $transfer['company']['company_email'] ?></td></tr>
+                </table>
+            </td>
+            <td>
+                <table cellpadding="2" class="table">
+                    <tr><td><h3>Details<br></h3></td></tr>
+                    <tr><td>Invoice number: <?=$transfer['transfer_id']?></td></tr>
+                    <tr><td>Issue date: <?=date('F d,Y',strtotime($transfer['invoice_date']))?></td></tr>
+                    <tr><td>Payment terms: Due immediately</td></tr>
+                    <tr><td><h5 style="margin-bottom:0; font-weight:bold; border-bottom:1px solid blue; padding: 1.85714286em;">Amount paid in KWD: <?= $transfer['company_total'] ?></h5></td></tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</div>
+<div class="row" style="border:1px solid #ececec;border-radius:6px; position: relative;overflow: hidden;padding: 1.85714286em;margin-bottom: 30px;">
+    <table class="table" cellpadding="3" cellspacing="3">
+        <tr>
+            <td align="left" style="text-align: left">
+                <span><b><?=number_format($totalHours)?> hours</b> worked x <b>2 KD</b> per hour</span>
+            </td>
+            <td align="right" style="text-align: right">
+                <span>KWD <?=number_format($totalAmount)?></span>
+            </td>
+        </tr>
+        <tr>
+            <td align="left" style="text-align: left">
+                <span><b>Bonus to be sent to interns</b></span>
+            </td>
+            <td align="right" style="text-align: right">
+                <span>KWD <?=number_format($totalBonus)?></span>
+            </td>
+        </tr>
+    </table>
+    <hr/>
+    <table class="table" >
+        <tr>
+            <td align="left" style="text-align: left">
+                <span class="h5" style="font-size: 1em;line-height: 1.85714286em;">Amount paid for <?=count($transfer['candidates'])?> interns</span>
+            </td>
+            <td align="right" style="text-align: right">
+                <span class="h5">KWD <?= $transfer['company_total'] ?></span>
+            </td>
+        </tr>
+    </table>
+</div>
+<!-- end of summary -->
+<div class="col-xs-12" style="">
+    <p style="margin-bottom:0;">Thank you for your business. It’s a pleasure to working with you.</p>
+    <p style="margin-bottom:0;">Sincerely yours,</p>
+    <p style="margin-bottom:0;">
+        Khalid Al-Mutawa<br/>
+        <img src="/images/signature.png" style="width:150px; display:block;">
+    </p>
+</div>
+<br/>
+<br/>
+<!-- end of summary -->
+<div class="col-xs-12" style="">
+    <h3>Bank Info</h3><br>
+    <p>BAWES FOR COMPUTER PROGRAMMING AND WEBSITE DESIGN AND DEVELOPMENT COMPANY</p>
+    <p>    شركة باوس لبرمجة وتشغيل الكمبيوتر وتصميم وادارة مواقع الانترنت</p>
+    <p>National Bank of Kuwait</p>
+    <p>IBAN: KW07NBOK0000000000002009288593</p>
+    <p>Account #: 2009288593</p>
+    <p>Swift: NBOKKWKW</p>
+</div>
