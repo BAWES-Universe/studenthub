40ba2da127d889776e495f997b35c92e8a1ae9a4
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-05-16 18:30:19 +0530

    transfer stats

--- common/models/Transfer.php
+++ common/models/Transfer.php
@@ -36,12 +36,12 @@ class Transfer extends \yii\db\ActiveRecord
     public function statusList()
     {
         return [
-            STATUS_PAYMENT_SENT => 'Payment Sent',
-            STATUS_PAYMENT_RECEIVED => 'Payment Received',
-            STATUS_SALARY_DISTRIBUTION_IN_PROGRESS => 'Salary distribution in progress',
-            STATUS_TRANSFER_COMPLETE => 'Transfer Completed',
-            STATUS_LOCK => 'Locked',
-            STATUS_INITIATED => 'Draft'
+            self::STATUS_PAYMENT_SENT => 'Payment Sent',
+            self::STATUS_PAYMENT_RECEIVED => 'Payment Received',
+            self::STATUS_SALARY_DISTRIBUTION_IN_PROGRESS => 'Salary distribution in progress',
+            self::STATUS_TRANSFER_COMPLETE => 'Transfer Completed',
+            self::STATUS_LOCK => 'Locked',
+            self::STATUS_INITIATED => 'Draft'
         ];
     }
 
