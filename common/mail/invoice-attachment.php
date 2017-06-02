293f90b997725740483eb3b8d8c2f34c3789f014
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-06-02 20:57:54 +0530

    receipt

---
+++ common/mail/invoice-attachment.php
@@ -0,0 +1,19 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $user common\models\User */
+
+?>
+<div class="password-reset">
+    <p>Hello,</p>
+
+    <br />
+
+    <p>Please find the attached invoice for your payment in order to proceed with the transfers.</p>
+
+	<br />
+
+	<p>Sincerely yours,<br />
+	Khalid Al-Mutawa</p>
+</div>
---
+++ common/mail/receipt-attachment.php
@@ -0,0 +1,20 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $user common\models\User */
+
+?>
+<div class="password-reset">
+    <p>Hello,</p>
+
+    <br />
+
+    <p>Please find attached your receipt for invoice #<?= $detail['invoice_id'] ?>.</p>
+	<p>Thank you for your business. It’s a pleasure working with you.</p>
+
+	<br />
+
+	<p>Sincerely yours,<br />
+	Khalid Al-Mutawa</p>
+</div>
