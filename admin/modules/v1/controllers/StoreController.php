2578b45360bfe4a5c4c6fb44bfd72a9fd8560df3
Anil Kumar <anilkumar.dhiman1@gmail.com>
2017-06-16 11:48:44 +0530

    admin api change for admin restriction on company and store deletion

--- admin/modules/v1/controllers/StoreController.php
+++ admin/modules/v1/controllers/StoreController.php
@@ -173,9 +173,7 @@ class StoreController extends Controller
 
         //Shouldn't be able to delete a store that has candidates assigned to it
 
-        $candidates = candidate::findOne(['store_id' => $store->store_id]);
-
-        if($candidates) {
+        if(count($store->candidates)>0) {
             return [
                 "operation" => "error",
                 "message" => "Store have some candidates assigned to it"
