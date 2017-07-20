95030bd7490ff68da749eb8b6894a684c9389d41
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-07-20 20:48:42 +0530

    bank test unit class

---
+++ common/tests/_data/bank.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    [
+        'bank_name' => 'Canara Bank',
+        'bank_swift_code' => 'SW275045',
+        'bank_address' => '24 Rose road, Maldiv, USA', 
+        'bank_transfer_type' => 'SWF'
+    ],
+];
