c4c0df3d5bec9a93ea927e82804df5fb30a519c2
Anil Kumar <anilkumar.dhiman1@gmail.com>
2017-07-26 14:22:10 +0530

    staff token model test cases created

---
+++ common/tests/_data/staffToken.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    [
+        'token_id' => 1,
+        'staff_id' => 1,
+        'token_value' => 'iCV2R0rTrBrcGodLz1SV-dLgc6Cv4u7t',
+        'token_device' => null,
+        'token_device_id' => null,
+        'token_status' => 1,
+        'token_last_used_datetime' => '2017-06-17 15:51:54',
+        'token_expiry_datetime' => '2017-06-17 15:51:54',
+        'token_created_datetime' => '2017-06-17 15:51:54',
+    ],
+];
