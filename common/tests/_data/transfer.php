fa7c8a4b18fcb99d5d219b75fe2698da47fdfb76
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-07-26 18:01:51 +0530

    common transfer model test class

---
+++ common/tests/_data/transfer.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*[
+        'transfer_id' => 1,
+        'parent_transfer_id' => 0,
+        'company_id' => 1,
+        'total' => 0,
+        'company_total' => 0,
+        'payment_received_on' => null,
+        'transfer_status' => 10,
+        'transfer_created_at' => '2017-07-12 18:15:20',
+        'transfer_updated_at' => '2017-07-12 18:15:20'
+    ]*/
+];
