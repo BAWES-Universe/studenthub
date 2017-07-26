72abee830c665a3c46ebfedf742f3728a45dc470
Anil Kumar <anilkumar.dhiman1@gmail.com>
2017-07-26 11:11:25 +0530

    invoice transfer and transfer candidate fixture added

---
+++ common/tests/_data/invoice.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    [
+    	'invoice_id' => 1,
+    	'transfer_id' => 2,
+    	'invoice_date' => '2017-07-19',
+    	'invoice_status' => 'unpaid',
+    	'deleted' => '0',
+    ]
+];
---
+++ common/tests/_data/transfer_candidate.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    [
+    	'tc_id' => 1,
+    	'transfer_id' => '1',
+    	'candidate_id' => '1',
+    	'store_id' => '1',
+    	'store_name' => 'First Store',
+    	'company_id' => '2',
+    	'company_name' => 'Second Company - Child',
+    	'company_email' => 'company@company.com',
+    	'candidate_hourly_rate' => '1.7',
+    	'company_hourly_rate' => '2.0',
+    	'hours' => 10,
+    	'bonus' => 10,
+    	'transfer_cost' => '0.350',
+    	'paid' => 0,
+    	'tc_created_at' => '2017-02-23 18:04:42',
+    	'tc_updated_at' => '2017-02-23 18:04:42'
+    ],
+    [
+        'tc_id' => 2,
+        'transfer_id' => '1',
+        'candidate_id' => '2',
+        'store_id' => '2',
+        'store_name' => 'Second Store',
+        'company_id' => '2',
+        'company_name' => 'Second Company - Child',
+        'company_email' => 'company@company.com',
+        'candidate_hourly_rate' => '1.5',
+        'company_hourly_rate' => '2.0',
+        'hours' => 20,
+        'bonus' => 18,
+        'transfer_cost' => '0.350',
+        'paid' => 0,
+        'tc_created_at' => '2017-02-23 18:04:42',
+        'tc_updated_at' => '2017-02-23 18:04:42'
+    ],
+];
