1fb0d05f966dd9a5c253574ca3d7f60ac15bb2df
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-08-01 20:27:02 +0530

    Admin and AdminToken model test class

---
+++ common/fixtures/AdminToken.php
@@ -0,0 +1,9 @@
+<?php
+namespace common\fixtures;
+
+use yii\test\ActiveFixture;
+
+class AdminToken extends ActiveFixture
+{
+    public $modelClass = 'common\models\AdminToken';
+}
---
+++ common/tests/unit/models/AdminTokenTest.php
@@ -0,0 +1,70 @@
+<?php
+namespace common\tests;
+
+use Codeception\Specify;
+use common\models\Admin;
+use common\models\AdminToken;
+use common\fixtures\Admin as AdminFixture;
+use common\fixtures\AdminToken as AdminTokenFixture;
+
+class AdminTokenTest extends \Codeception\Test\Unit
+{
+    use Specify;
+    
+    /**
+     * @var \common\tests\UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+        $this->tester->haveFixtures([
+            'admin' => [
+                'class' => AdminFixture::className(),
+                'dataFile' => codecept_data_dir() . 'admin.php'
+            ],
+            'adminToken' => [
+                'class' => AdminTokenFixture::className(),
+                'dataFile' => codecept_data_dir() . 'adminToken.php'
+            ]
+        ]);
+    }
+
+    protected function _after(){}
+
+    // tests
+    public function testValidation()
+    {
+        $this->specify('Fixtures should be loaded', function() {
+            expect('Admin is in the table', Admin::find()->one())->notNull();
+            expect('Admin Token is in the table', AdminToken::find()->one())->notNull();
+        });
+
+        $this->specify('Test Validator', function() {
+            $model = new AdminToken();
+            $model->validate();
+            expect('admin_id required error',$model->errors)->hasKey('admin_id');
+            expect('token_value required error',$model->errors)->hasKey('token_value');
+            expect('token_status required error',$model->errors)->hasKey('token_status');
+            expect('total 3 errors',count($model->errors))->equals(3);
+        });
+    }
+
+    /**
+     * testing generate token
+     * testing relating data
+     */
+    public function testGenerateToken()
+    {
+        $this->specify('Fixtures should be loaded', function() {
+            expect('Admin Token is in the table', AdminToken::find()->one())->notNull();
+        });
+
+        $this->specify('Test existing Token', function() {
+            expect(
+                'unique token string', 
+                AdminToken::findOne(['token_value' => AdminToken::generateUniqueTokenString()])
+            )->null();
+        });
+    }
+}
