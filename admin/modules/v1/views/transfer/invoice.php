55f1be2f873838c66429a953e9e671b1f7c30bf9
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-08-07 20:06:19 +0530

    invoice receipt transfer id

--- admin/modules/v1/views/transfer/invoice.php
+++ admin/modules/v1/views/transfer/invoice.php
@@ -31,8 +31,8 @@ foreach ($invoice->transfer->transferCandidates as $key => $value) {
             <td>
                 <table cellpadding="2" class="table">
                     <tr><td><h3 style="font-weight: 100;">Details<br></h3></td></tr>
-                    <tr><td>Invoice number: <?=$invoice->invoice_id?></td></tr>
-                    <tr><td><p>Transfer number: <?=$invoice->transfer_id?></p></td></tr>
+                    <tr><td>Invoice number: <?= $invoice->invoice_id ?></td></tr>
+                    <tr><td><p>Transfer number: <?= $invoice->transfer->parent_transfer_id? $invoice->transfer->parent_transfer_id : $invoice->transfer->transfer_id ?></p></td></tr>
                     <tr><td><p>Issue date: <?=date('F d,Y',strtotime($invoice->invoice_date))?></p></td></tr>
                     <tr><td><p>Payment terms: Due immediately</p></td></tr>
                     <tr><td><h5 style="margin-bottom:0; font-weight:bold; border-bottom:1px solid blue; padding: 1.85714286em;">Amount due in KWD: <?= number_format($invoice->transfer->company_total, 3) ?></h5></td></tr>
