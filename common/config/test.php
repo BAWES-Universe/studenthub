24cb2d72a8bed71d03470591d2b567e380e2c25f
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-08-14 18:46:00 +0530

    common config for test

--- common/config/test.php
+++ common/config/test.php
@@ -1,5 +1,6 @@
 <?php
 return [
-    'id' => 'app-common-tests',
-    'basePath' => dirname(__DIR__),
+    'name' => 'StudentHub Internship Program - Test',
+    'timeZone' => 'Asia/Kuwait',
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
 ];
