07d10d091dfa4fe932b3a0ec6970e5e72edf4f23
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-08-17 19:12:16 +0530

    candidate id card functional test class

---
+++ staff/fixtures/CandidateIdCard.php
@@ -0,0 +1,9 @@
+<?php
+namespace staff\fixtures;
+
+use yii\test\ActiveFixture;
+
+class CandidateIdCard extends ActiveFixture
+{
+    public $modelClass = 'staff\models\CandidateIdCard';
+}
---
+++ staff/tests/functional/CandidateIdCardCest.php
@@ -0,0 +1,126 @@
+<?php
+namespace staff\tests;
+
+use yii;
+use yii\helpers\ArrayHelper;
+use common\models\StaffToken;
+use staff\models\Candidate;
+use staff\fixtures\staff as StaffFixture;
+use staff\fixtures\StaffToken as StaffTokenFixture;
+use staff\fixtures\Candidate as CandidateFixture;
+use staff\fixtures\CandidateIdCard as CandidateIdCardFixture;
+use Codeception\Util\HttpCode;
+
+class CandidateIdCardCest
+{    
+    public $token;
+
+    public function _before(FunctionalTester $I)
+    {   
+        Yii::$app->params['inCodeception'] = true;
+        
+        $I->haveFixtures([
+            'staff' => [
+                'class' => StaffFixture::className(),
+                'dataFile' => Yii::getAlias('@common').'/tests/_data/staff.php'
+            ],
+            'staffToken' => [
+                'class' => StaffTokenFixture::className(),
+                'dataFile' => Yii::getAlias('@common').'/tests/_data/staffToken.php'
+            ],
+            'candidate' => [
+                'class' => CandidateFixture::className(),
+                'dataFile' => Yii::getAlias('@common').'/tests/_data/candidate.php'
+            ],
+            'candidateIdCard' => [
+                'class' => CandidateIdCardFixture::className(),
+                'dataFile' => Yii::getAlias('@common').'/tests/_data/candidateIdCard.php'
+            ]
+        ]);
+        
+        $this->token = StaffToken::find()
+            ->one()
+            ->token_value;
+         
+        $I->amBearerAuthenticated($this->token);
+    }
+
+    /**
+     * List Candidates having ID Cards
+     * @param FunctionalTester $I
+     */
+    public function listCandidatesHavingIdCards(FunctionalTester $I)
+    {
+        $I->wantTo('List Candidates having ID Cards');
+        $I->sendGET('v1/candidate-id-cards/list-candidate-ids?page=1&candidate_name=');
+        $I->seeResponseCodeIs(HttpCode::OK); 
+    }
+    
+    /**
+     * List Candidates to Generate ID Cards
+     * @param FunctionalTester $I
+     */
+    public function listCandidatesToGenerateIdCards(FunctionalTester $I)
+    {
+        $I->wantTo('List Candidates to Generate ID Cards');
+        $I->sendGET('v1/candidate-id-cards/list-candidates');
+        $I->seeResponseCodeIs(HttpCode::OK); 
+    }
+    
+    /**
+     * Generate ID Cards 
+     * @param FunctionalTester $I
+     */
+    public function tryToGenerateIdCards(FunctionalTester $I)
+    {
+        //candidate not having cards 
+        $candidates = Candidate::find()
+            ->where('candidate_id NOT IN(select candidate_id from candidate_id_card)')
+            ->all();
+        
+        $arrCandidates = ArrayHelper::map($candidates, 'candidate_id', 'candidate_id');
+        
+        $I->wantTo('Generate ID Cards');
+        $I->sendPOST('v1/candidate-id-cards/generate', [
+            'candidates' => $arrCandidates
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK); 
+    }
+    
+    /**
+     * List Expired ID
+     * @param FunctionalTester $I
+     */
+    public function listExpiredIdCards(FunctionalTester $I)
+    {
+        $I->wantTo('List Expired ID');
+        $I->sendGET('v1/candidate-id-cards/list-expired');
+        $I->seeResponseCodeIs(HttpCode::OK); 
+    }
+    
+    /**
+     * Renew ID Cards
+     * @param FunctionalTester $I
+     */
+    public function renewIDCards(FunctionalTester $I)
+    {
+        $arrCandidates = [1, 2];
+        
+        $I->wantTo('Renew ID Cards');
+        $I->sendPOST('v1/candidate-id-cards/renew', [
+            'candidates' => $arrCandidates
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK); 
+    }
+    
+    /**
+     * Total Expired ID
+     * @param FunctionalTester $I
+     */
+    public function getTotalExpiredIdCards(FunctionalTester $I)
+    {
+        $I->wantTo('List Expired ID');
+        $I->sendGET('v1/candidate-id-cards/total-expired');
+        $I->seeResponseCodeIs(HttpCode::OK); 
+    }
+}
