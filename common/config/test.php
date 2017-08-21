54a978d2cb1b33666e99d1382b31a135ce2b3d94
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-08-21 18:04:37 +0530

    codecept run error on common folder

--- common/config/test.php
+++ common/config/test.php
@@ -1,6 +1,7 @@
 <?php
 return [
-    'name' => 'StudentHub Internship Program - Test',
+    'id' => 'StudentHub Internship Program - Test',
     'timeZone' => 'Asia/Kuwait',
+    'basePath' => dirname(__DIR__),
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
 ];
