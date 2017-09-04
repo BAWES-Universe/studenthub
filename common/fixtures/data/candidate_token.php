983be0f17d87ff36a6975810a0e3fae2d644e87d
Khalid <khalid@bawes.net>
2017-09-04 17:19:48 +0300

    Faker template to generate candidate tokens

--- common/fixtures/data/candidate_token.php
+++ common/fixtures/data/candidate_token.php
@@ -2,14 +2,23 @@
 
 return [
     [
-        'token_id' => 1,
         'candidate_id' => 1,
-        'token_value' => 'HDFXJLjeI43y2r9hzHq7pcMnNUE3T2n3',
+        'token_value' => 'OJUh2oKfkaJ3kCckBWT1zMbvjCRy6st6',
         'token_device' => null,
         'token_device_id' => null,
         'token_status' => 1,
-        'token_last_used_datetime' => '2017-06-17 15:51:54',
-        'token_expiry_datetime' => '2017-06-17 15:51:54',
-        'token_created_datetime' => '2017-06-17 15:51:54',
+        'token_last_used_datetime' => '1971-06-21T13:15:50+0300',
+        'token_expiry_datetime' => '1999-11-06T07:46:21+0300',
+        'token_created_datetime' => '1971-10-19T07:16:14+0300',
+    ],
+    [
+        'candidate_id' => 2,
+        'token_value' => 'DbnV-CrDz5bPyZPiY1BBnSWNCeYjMaGN',
+        'token_device' => null,
+        'token_device_id' => null,
+        'token_status' => 1,
+        'token_last_used_datetime' => '1980-08-29T05:14:23+0300',
+        'token_expiry_datetime' => '1975-03-28T14:37:00+0300',
+        'token_created_datetime' => '2011-10-17T15:16:40+0300',
     ],
 ];
