e76392bb6648cdf26bd9f39c6bbd28777dd62e26
Krushn <kathrechakrushn@gmail.com>
2020-07-14 20:08:24 +0530

    functional test fixed for transfer

--- common/components/S3ResourceManager.php
+++ common/components/S3ResourceManager.php
@@ -93,7 +93,8 @@ class S3ResourceManager extends Component {
         if ($file) {
             $source_file = $file->tempName;
             $content_type = $file->type;
-        } else {
+            
+        } else if(strpos ($source_file, 'http') > -1) { //if url
             $source_file = urlencode($source_file);
         }
 
