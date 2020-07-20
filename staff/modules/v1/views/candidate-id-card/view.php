3c504851099a5735c0a808a14a3655c3dbe6d537
Krushn <kathrechakrushn@gmail.com>
2020-07-20 20:21:18 +0530

    id card font + padding margin updated

--- staff/modules/v1/views/candidate-id-card/view.php
+++ staff/modules/v1/views/candidate-id-card/view.php
@@ -2,7 +2,7 @@
 
 mb_internal_encoding("UTF-8");
 
-$nameSections = mb_split( ' ',$model->candidate->candidate_name_ar);
+$nameSections = mb_split(' ', $model->candidate->candidate_name_ar);
 
 ?>
 <html>
@@ -10,55 +10,76 @@ $nameSections = mb_split( ' ',$model->candidate->candidate_name_ar);
         <title>ID Card</title>
         <style>
 
-            /*
-             * Droid Arabic Kufi (Arabic) http://www.google.com/fonts/earlyaccess
-             */
-             @font-face {
+            @font-face {
+              font-family: 'effra';
+              src: url('<?= Yii::getAlias("@web") ?>/fonts/effra_std_bd-webfont.woff2') format('woff2'),
+                   url('<?= Yii::getAlias("@web") ?>/fonts/effra_std_bd-webfont.woff') format('woff'),
+                   url('<?= Yii::getAlias("@web") ?>/fonts/effra_std_bd-webfont.ttf') format('truetype');
+              font-weight: 700;
+              font-style: normal;
+            }
+
+            @font-face {
+              font-family: 'effra';
+              src: url('<?= Yii::getAlias("@web") ?>/fonts/effra_std_rg-webfont.woff2') format('woff2'),
+                   url('<?= Yii::getAlias("@web") ?>/fonts/effra_std_rg-webfont.woff') format('woff'),
+                   url('<?= Yii::getAlias("@web") ?>/fonts/effra_std_rg-webfont.ttf') format('truetype');
+              font-weight: 400;
+              font-style: normal;
+            }
+
+            @font-face {
+              font-family: 'effra';
+              src: url('<?= Yii::getAlias("@web") ?>/fonts/l') format('woff2'),
+                   url('<?= Yii::getAlias("@web") ?>/fonts/d.woff') format('woff'),
+                   url('<?= Yii::getAlias("@web") ?>/fonts/a') format('opentype');
+              font-weight: 500;
+              font-style: normal;
+            }
+
+            @font-face {
               font-family: 'Droid Arabic Kufi';
               font-style: normal;
               font-weight: 400;
-              src: url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Regular.eot);
-              src: url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Regular.eot?#iefix) format('embedded-opentype'),
-                   url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Regular.woff2) format('woff2'),
-                   url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Regular.woff) format('woff'),
-                   url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Regular.ttf) format('truetype');
+              src: url(<?= Yii::getAlias("@web") ?>/fonts/Droid-Arabic-Kufi/DroidKufi-Regular.ttf) format('truetype');
             }
 
             @font-face {
               font-family: 'Droid Arabic Kufi';
               font-style: normal;
               font-weight: 700;
-              src: url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Bold.eot);
-              src: url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Bold.eot?#iefix) format('embedded-opentype'),
-                   url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Bold.woff2) format('woff2'),
-                   url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Bold.woff) format('woff'),
-                   url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Bold.ttf) format('truetype');
+              src: url(<?= Yii::getAlias("@web") ?>/fonts/Droid-Arabic-Kufi/DroidKufi-Bold.ttf) format('truetype');
             }
 
             html,body {
                 font-family: 'Droid Arabic Kufi', 'effra', sans-serif;
                 background: #fff;padding:0;margin:0;
             }
-            .front-card {width:638px; height:1011px; float:left;margin-right: 44px;margin-bottom: 100px;}
+            
+            :focus, a:focus {
+                outline: 0
+            }
+            .front-card {width:638px; height:1011px; float:left;margin-bottom: 100px;}
             .top-part{background: #3D4AB8;min-height:599px;text-align: center;position: relative;}
-            .code{position: absolute;left: 18px;writing-mode: vertical-rl;text-orientation: mixed;top: 40px;font-size: 36px;letter-spacing: normal;color: #ffffff;}
-            .image{width: 319px;overflow: hidden;border-radius: 50%;position: absolute;top: 10%;left: 23%;border: 10px solid #fff;}
-            .name-top{position: absolute; right: 69px; bottom: 25px; font-family: DroidArabicKufi;   font-size: 64px;   font-weight: bold;   font-stretch: normal;   font-style: normal;   line-height: normal;   letter-spacing: normal;   color: #ffffff;}
-            .name-bottom{position: absolute; right: 69px; top: 25px; font-family: DroidArabicKufi;   font-size: 64px;   font-weight: bold;   font-stretch: normal;   font-style: normal;   line-height: normal;   letter-spacing: normal;   color: #000000;}
+            .code{position: absolute;left: 10px;writing-mode: vertical-rl;text-orientation: mixed;top: 30px;font-size: 36px;letter-spacing: normal;color: #ffffff;}
+            .image{width: 319px; height: 319px;overflow: hidden;border-radius: 50%;position: absolute;top: 98px;left: calc((100% - 319px)/2);border: 5px solid #fff;}
+            .name-top{position: absolute; right: 69px; bottom: 25px;   font-size: 64px;   font-weight: bold;   font-stretch: normal;   font-style: normal;   line-height: normal;   letter-spacing: normal;   color: #ffffff;}
+            .name-bottom{position: absolute; right: 69px; top: 25px;  font-size: 55px;  font-stretch: normal;   font-style: normal;   line-height: normal;   letter-spacing: normal;   color: #000000;}
             .bottom-part{background: #fff; height: 412px; position: relative;}
-            .back-card {width:638px;height:1011px;float: left;background: #fff;margin-bottom: 100px;position: relative;}
+            .back-card {width:638px;height:1011px;float: left;background: #fff;margin-bottom: 0;position: relative;}
             .qr-code {text-align: center;padding-top: 93px;}
             .qr-code h3 {font-size: 56px;margin-top: 18px;font-weight: normal;color: #2A2728;}
             .address{text-align: right;margin-right: 50px;}
             .contact-detail{line-height: 0;font-size: 44px;margin-top: 170px;}
-            .secondry-address{font-size: 24px;line-height: 0.59;margin-top: 36px;}
-            .main-address{font-size: 50px;margin-top: 108px;}
+            .secondry-address{font-size: 24px;line-height: 1.59;margin-top: 50px;}
+            .secondry-address p { margin: 0px; }
+            .main-address{font-size: 50px;margin-top: 85px;}
             .uni-label{margin: 0;  font-size: 36px;  font-weight: bold;}
-            .uni-data{margin: 0;font-size: 36px;font-weight: normal;}
+            .uni-data{margin: -9px 0 -4px 0;font-size: 36px;font-weight: normal;}
             .civil-lbl{margin-bottom: 0;font-weight: bold;font-size: 36px;margin-top: 9px;}
-            .logo-1 {position: absolute;bottom: 61px;left: calc((100% - 437px)/2);}
-            .qr{width: 367px;margin: 0 auto;padding: 53px 0;}
-            .qr img{width: 74%;}
+            .logo-1 {position: absolute;bottom: 61px;left: calc((100% - 403px)/2);}
+            .qr{width: 367px;margin: 0 auto;padding: 0;}
+            .qr img{width: 289px; height: 289px}
         </style>
     </head>
     <body>
@@ -102,7 +123,7 @@ $nameSections = mb_split( ' ',$model->candidate->candidate_name_ar);
                     <p class="uni-label">طالب</p>
                     <p class="uni-data"><?= $model->candidate->university->university_name_ar ?></p>
                     <p class="civil-lbl">الرقم المدني</p>
-                    <p style="margin: 0;font-size: 36px;"><?=$model->candidate->candidate_civil_id;?></p>
+                    <p style="margin: -7px 0 0 0;font-size: 36px;"><?=$model->candidate->candidate_civil_id;?></p>
                 </div>
                 <div class="secondry-address">
                     <p>إذا تم العثور على هذه البطاقة</p>
@@ -113,7 +134,7 @@ $nameSections = mb_split( ' ',$model->candidate->candidate_name_ar);
 <!--                    <p>contact@bawes.net</p>-->
 <!--                    <p>+965 98009771</p>-->
 <!--                </div>-->
-                <?php echo \yii\helpers\Html::img('@web/img/back-logo.svg',['style'=>'position: absolute;left: 80px;left: 36px;bottom: 34px'])?>
+                <?php echo \yii\helpers\Html::img('@web/img/back-logo.svg',['style'=>'position: absolute;left: 35px;bottom: 45px'])?>
             </div>
         </div>
 
