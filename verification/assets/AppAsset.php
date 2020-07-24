451ee02c4f5939b41c9f4531d8f0c0f1b54da0cd
Krushn <kathrechakrushn@gmail.com>
2020-07-24 19:56:05 +0530

    extra assets removed

--- verification/assets/AppAsset.php
+++ verification/assets/AppAsset.php
@@ -13,32 +13,12 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/bootstrap.css',
-        'css/stack-interface.css',
-        'css/socicon.css',
-        'css/lightbox.min.css',
-        'css/flickity.css',
         'css/theme.css',
         'css/custom.css',
         'https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400i,500,600,700%7CMerriweather:300,300i%7CMaterial+Icons'
     ];
     public $js = [
-        'js/flickity.min.js',
-        'js/easypiechart.min.js',
-        'js/parallax.js',
-        'js/typed.min.js',
-        'js/datepicker.js',
-        'js/isotope.min.js',
-        'js/ytplayer.min.js',
-        'js/lightbox.min.js',
-        'js/granim.min.js',
-        'js/countdown.min.js',
-        'js/twitterfetcher.min.js',
-        'js/spectragram.min.js',
-        'js/smooth-scroll.min.js',
-        'js/scripts.js'
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
     ];
 }
