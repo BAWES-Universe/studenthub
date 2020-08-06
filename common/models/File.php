8a3d393b0459ab93488f5e7eafd9c72c0d3bf458
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-08-06 16:15:11 +0530

    [Finishes #174088352] file migration and model created

---
+++ common/models/File.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\behaviors\TimestampBehavior;
+use yii\behaviors\AttributeBehavior;
+use yii\db\Expression;
+
+
+/**
+ * This is the model class for table "file".
+ *
+ * @property string $file_uuid
+ * @property int $company_id
+ * @property string $file_title
+ * @property string $file_description
+ * @property string $file_name
+ * @property string $file_type
+ * @property int $file_size
+ * @property string $file_s3_path
+ * @property string $file_created_datetime
+ *
+ * @property Company $company
+ */
+class File extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'file';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['company_id', 'file_size'], 'integer'],
+            [['file_title', 'file_created_datetime'], 'required'],
+            [['file_description'], 'string'],
+            [['file_created_datetime'], 'safe'],
+            [['file_uuid'], 'string', 'max' => 60],
+            [['file_title', 'file_name'], 'string', 'max' => 255],
+            [['file_type'], 'string', 'max' => 10],
+            [['file_s3_path'], 'string', 'max' => 225],
+            [['company_id'], 'exist', 'skipOnError' => true, 'targetClass' => Company::className(), 'targetAttribute' => ['company_id' => 'company_id']],
+        ];
+    }
+
+    public function behaviors() {
+        return [
+            [
+                'class' => AttributeBehavior::className(),
+                'attributes' => [
+                    \yii\db\ActiveRecord::EVENT_BEFORE_INSERT => 'file_uuid',
+                ],
+                'value' => function() {
+                    if(!$this->file_uuid)
+                        $this->file_uuid = 'file_' . Yii::$app->db->createCommand('SELECT uuid()')->queryScalar();
+
+                    return $this->file_uuid;
+                }
+            ],
+            [
+                'class' => TimestampBehavior::className(),
+                'createdAtAttribute' => 'file_created_datetime',
+                'updatedAtAttribute' => false,
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'file_uuid' => 'File Uuid',
+            'company_id' => 'Company ID',
+            'file_title' => 'File Title',
+            'file_description' => 'File Description',
+            'file_name' => 'File Name',
+            'file_type' => 'File Type',
+            'file_size' => 'File Size',
+            'file_s3_path' => 'File S3 Path',
+            'file_created_datetime' => 'File Created Datetime',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCompany()
+    {
+        return $this->hasOne(Company::className(), ['company_id' => 'company_id']);
+    }
+}
