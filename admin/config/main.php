71b8d15dde0175fa44e1c1d254b739bc7e08b65a
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-08-17 16:42:09 +0530

    [Finishes #174323435] company enable disable option is working now

--- admin/config/main.php
+++ admin/config/main.php
@@ -89,6 +89,7 @@ return [
                         'POST file-create/<id>' => 'create-file',
                         'PATCH <id>' => 'update',
                         'PATCH reset-password/<id>' => 'reset-password',
+                        'PATCH change-status/<id>' => 'change-status',
                         'DELETE <id>' => 'delete',
                         'DELETE remove-file/<id>' => 'delete-file',
                         // OPTIONS VERBS
@@ -98,6 +99,7 @@ return [
                         'OPTIONS reset-password/<id>' => 'options',
                         'OPTIONS file-create/<id>' => 'options',
                         'OPTIONS remove-file/<id>' => 'options',
+                        'OPTIONS change-status/<id>' => 'options',
                     ]
                 ],
                 [ // StoreController
