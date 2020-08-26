4f9b847dfe501917d9fb3e5413424a8d270e35e9
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-08-26 17:08:48 +0530

    [Finishes #174461600] confirmation is showing now

--- staff/modules/v1/controllers/RequestController.php
+++ staff/modules/v1/controllers/RequestController.php
@@ -149,7 +149,7 @@ class RequestController extends Controller
         $model->request_position_title = Yii::$app->request->getBodyParam("position_title");
         $model->request_number_of_employees = Yii::$app->request->getBodyParam("number_of_employees");
         $model->request_additional_info = Yii::$app->request->getBodyParam("additional_info");
-        
+
         if (!$model->save())
         {
             if(isset($model->errors)){
@@ -213,7 +213,8 @@ class RequestController extends Controller
         $model = $this->findModel($id);
         
         $model->request_status = Request::STATUS_DELIVERED;
-        
+        $model->request_feedback = Yii::$app->request->getBodyParam("feedback");
+
         if (!$model->save())
         {
             if(isset($model->errors)){
@@ -245,7 +246,8 @@ class RequestController extends Controller
         $model = $this->findModel($id);
         
         $model->request_status = Request::STATUS_CANCELLED;
-        
+        $model->request_feedback = Yii::$app->request->getBodyParam("feedback");
+
         if (!$model->save())
         {
             if(isset($model->errors)){
