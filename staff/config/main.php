c89e9402679c8299777956e582d5585f87a84d07
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-09-09 16:20:43 +0530

    [Finishes #174697845] brand crud is added

--- staff/config/main.php
+++ staff/config/main.php
@@ -265,6 +265,20 @@ return [
                         'OPTIONS deliver/<id>' => 'options',
                     ]
                 ],
+                [ // BrandController
+                    'class' => 'yii\rest\UrlRule',
+                    'controller' => 'v1/brand',
+                    'patterns' => [
+                        'GET' => 'list',
+                        'GET <id>' => 'view',
+                        'POST' => 'create',
+                        'PATCH <id>' => 'update',
+                        'DELETE <id>' => 'delete',
+                        // OPTIONS VERBS
+                        'OPTIONS' => 'options',
+                        'OPTIONS <id>' => 'options',
+                    ]
+                ],
             ],
         ],
     ],
