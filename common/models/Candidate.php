1299f78bed71d131d2acc282ca21034b6af9773b
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-11-24 16:14:27 +0530

    [Finishes #174408238] bank detail issue should show error msg if iban code is invalid

--- common/models/Candidate.php
+++ common/models/Candidate.php
@@ -155,7 +155,7 @@ class Candidate extends \yii\db\ActiveRecord implements \yii\web\IdentityInterfa
             [
                 ['bank_account_name', 'candidate_name', 'candidate_name_ar'], 'validateFullName'
             ],
-           // ['candidate_iban', 'validateIban'],
+            ['candidate_iban', 'validateIban'],
             ['candidate_hourly_rate', 'validateHourlyRate'],
             [['candidate_civil_expiry_date'], 'validateCivilExpiry'],
             [['candidate_password_reset_token'], 'unique'],
