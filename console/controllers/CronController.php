ce1624823ab0d8108e705cae58824f34842dd192
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-11-24 15:00:42 +0530

    [Finishes #175399677] payable candidate changes done

--- console/controllers/CronController.php
+++ console/controllers/CronController.php
@@ -156,6 +156,7 @@ class CronController extends \yii\console\Controller {
      * Implement cron function that checks if Payable candidates with bank info avail > 0,
      * it sends an email to khalid@bawes.net telling to process transfer.
      *  Confirm values/excel shown are only for those candidates which are payable and bank info avail.
+     * php yii cron/payable-candidate-notification
      */
     public function actionPayableCandidateNotification()
     {
@@ -172,6 +173,7 @@ class CronController extends \yii\console\Controller {
                 }
             }
         }
+        $amount = number_format($amount, 3);
 
         if ($candidates && count($candidates) > 0) {
 
@@ -229,7 +231,7 @@ class CronController extends \yii\console\Controller {
 
             $extension = pathinfo($file, PATHINFO_EXTENSION);
 
-            $subject = "Payable candidates Detail";
+            $subject = "We need to process KWD $amount to ".count($candidates)." people";
 
             if (YII_ENV != 'prod') {
                 $subject = '[Fake] [Ignore] ' . $subject;
