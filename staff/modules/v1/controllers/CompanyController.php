3eef88da90110195d814391224d3bd474195a3bd
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-12-16 11:06:35 +0530

    [Finishes #176132177,#176132315,#176133804] company filter changes done. css issue fixed. full timer nav disable is done

--- staff/modules/v1/controllers/CompanyController.php
+++ staff/modules/v1/controllers/CompanyController.php
@@ -74,8 +74,6 @@ class CompanyController extends Controller
     {
         $status = Yii::$app->request->getQueryParam("status",0);
         $name = Yii::$app->request->getQueryParam("name",0);
-        $common_name_en = Yii::$app->request->getQueryParam("common_name_en",0);
-        $common_name_ar = Yii::$app->request->getQueryParam("common_name_ar",0);
         
         $query = Company::find()
             ->filterParent();
@@ -94,14 +92,6 @@ class CompanyController extends Controller
 
         if ($name) {
             $query->filterByName($name);
-        }
-
-        if ($common_name_en) {
-            $query->filterByNameEn($common_name_en);
-        }
-
-        if ($common_name_ar) {
-            $query->filterByNameAr($common_name_ar);
         }
 
         return new ActiveDataProvider([
