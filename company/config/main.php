02d8dbbe4914de42e1ad86af790c22b4d49a7295
Krushn Kathrecha <kathrechakrushn@gmail.com>
2020-12-28 16:11:19 +0530

    api to get algolia key

--- company/config/main.php
+++ company/config/main.php
@@ -37,6 +37,16 @@ return [
             'enableStrictParsing' => true,
             'showScriptName' => false,
             'rules' => [
+                [ // AlgoliaController
+                    'class' => 'yii\rest\UrlRule',
+                    'controller' => 'v1/algolia',
+                    'pluralize' => false,
+                    'patterns' => [
+                        'GET key' => 'key',
+                        // OPTIONS VERBS
+                        'OPTIONS key' => 'options'
+                    ]
+                ],
                 [ // AuthController
                     'class' => 'yii\rest\UrlRule',
                     'controller' => 'v1/auth',
