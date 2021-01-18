e19a0ec287961b50de6b002167d30a214bd546b3
Anil Kumar <anilkumar.dhiman1@gmail.com>
2021-01-18 17:44:57 +0530

    text case fixed

--- common/tests/unit/models/TransferCandidateTest.php
+++ common/tests/unit/models/TransferCandidateTest.php
@@ -237,23 +237,25 @@ class TransferCandidateTest extends \Codeception\Test\Unit
         $this->specify('fixture data load test & test to check payable amount is with 3 digit after point', function () {
             $transferCandidateData = TransferCandidate::getPayableCandidateListFormat();
 
-            expect('if data exist', count($transferCandidateData['candidate_list']))->greaterThan(0);
+            if ($transferCandidateData && count($transferCandidateData['candidate_list']) >0 ) {
+                expect('if data exist', count($transferCandidateData['candidate_list']))->greaterThan(0);
 
 
-            $testingData = $transferCandidateData['candidate_list'][count($transferCandidateData)-1];
+                $testingData = $transferCandidateData['candidate_list'][count($transferCandidateData) - 1];
 
-            // testing for single candidate
-            list($whole, $decimal) = explode('.', $testingData['amount']);
-            list($whole1, $decimal1) = explode('.', $transferCandidateData['total_amount']);
+                // testing for single candidate
+                list($whole, $decimal) = explode('.', $testingData['amount']);
+                list($whole1, $decimal1) = explode('.', $transferCandidateData['total_amount']);
 
 
-            expect('length should be 3',strlen($decimal))->equals(3);
-            expect('length should be 3',strlen($decimal1))->equals(3);
+                expect('length should be 3', strlen($decimal))->equals(3);
+                expect('length should be 3', strlen($decimal1))->equals(3);
 
-            expect('with comma test case',strpos('11,11',','))->equals(2);
+                expect('with comma test case', strpos('11,11', ','))->equals(2);
 
-            expect('no comma in first value',strpos($whole,','))->false();
-            expect('no comma in first value',strpos($whole1,','))->false();
+                expect('no comma in first value', strpos($whole, ','))->false();
+                expect('no comma in first value', strpos($whole1, ','))->false();
+            }
 
         });
     }
