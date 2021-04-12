b1c3866f5119d831923a69b324555ada64776b5e
Anil Kumar <anilkumar.dhiman1@gmail.com>
2021-04-12 13:29:51 +0530

    [Finishes #177678369] token update done

--- common/models/StaffToken.php
+++ common/models/StaffToken.php
@@ -68,10 +68,15 @@ class StaffToken extends \yii\db\ActiveRecord
             [
                 'class' => TimestampBehavior::className(),
                 'createdAtAttribute' => 'token_created_datetime',
-                'updatedAtAttribute' => false,
+                'updatedAtAttribute' => 'token_last_used_datetime',
                 'value' => new Expression('NOW()'),
             ],
         ];
+    }
+
+    public function afterFind() {
+        $this->token_last_used_datetime =  new Expression('NOW()');
+        $this->save(false);
     }
 
     /**
