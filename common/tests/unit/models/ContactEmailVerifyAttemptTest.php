ca54794e2aa5d737390f97e63285cd547c88bb09
Krushn Kathrecha <kathrechakrushn@gmail.com>
2022-01-07 12:02:16 +0530

    fixed test

--- common/tests/unit/models/ContactEmailVerifyAttemptTest.php
+++ common/tests/unit/models/ContactEmailVerifyAttemptTest.php
@@ -25,15 +25,15 @@ class ContactEmailVerifyAttemptTest extends \Codeception\Test\Unit
     {
         $model = new \common\models\ContactEmailVerifyAttempt();
 
-        $model->candidate_email = null;
+        $model->email = null;
         $model->code = null;
         $model->ip_address = null;
 
-        expect('email should be required field', $model->validate(['candidate_email']))->false();
+        expect('email should be required field', $model->validate(['email']))->false();
         expect('code should be required field', $model->validate(['code']))->false();
         expect('ip address should be required field', $model->validate(['ip_address']))->false();
 
-        $model->candidate_email = '123123123';
-        expect('Invalid email', $model->validate(['candidate_email']))->false();
+        $model->email = '123123123';
+        expect('Invalid email', $model->validate(['email']))->false();
     }
 }
