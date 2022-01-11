629513976736a0732203a908538d1fde9a41deed
krushn <kathrechakrushn@gmail.com>
2022-01-11 17:19:08 +0530

    ability to know if request updated

--- company/config/main.php
+++ company/config/main.php
@@ -190,6 +190,7 @@ return [
                         'GET' => 'list',
                         'GET active' => 'list-active',
                         'GET count' => 'request-count',
+                        'GET is-request-updated/<id>' => 'is-request-updated',
                         'GET <id>' => 'view',
                         'POST' => 'create',
                         'PATCH <id>' => 'update',
@@ -197,6 +198,7 @@ return [
                         'OPTIONS' => 'options',
                         'OPTIONS count' => 'options',
                         'OPTIONS active' => 'options',
+                        'OPTIONS is-request-updated/<id>' => 'options',
                         'OPTIONS <id>' => 'options',
                     ]
                 ],
