8a281f8df31d89f1c267c74a5f323fc80144537a
Anil Kumar <anilkumar.dhiman1@gmail.com>
2022-12-01 12:24:09 +0530

    built issue

--- environments/circle-ci/common/config/main-local.php
+++ environments/circle-ci/common/config/main-local.php
@@ -3,14 +3,14 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=payroll',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=payroll_test',
             'username' => 'bawes',
             'password' => 'passw0rd',
             'charset' => 'utf8',
         ],
         'walletDb' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=payroll',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=payroll_test',
             'username' => 'bawes',
             'password' => 'passw0rd',
             'charset' => 'utf8',
