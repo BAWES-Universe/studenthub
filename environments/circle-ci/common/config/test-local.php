c2200244d31cf543573d92dbc309fd670d328503
Krushn Kathrecha <kathrechakrushn@gmail.com>
2023-03-15 11:55:13 +0530

    fixed tests

--- environments/circle-ci/common/config/test-local.php
+++ environments/circle-ci/common/config/test-local.php
@@ -7,7 +7,10 @@ return yii\helpers\ArrayHelper::merge(
         'components' => [
             'db' => [
                 'dsn' => 'mysql:host=127.0.0.1;dbname=payroll_test',
-            ]
+            ],
+            'walletDb' => [
+                'dsn' => 'mysql:host=127.0.0.1;dbname=wallet_test'
+            ],
         ],
     ]
 );
