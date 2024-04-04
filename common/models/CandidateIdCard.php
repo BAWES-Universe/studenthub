96d948123e93dbc5730c4b507bd0c2e2c99f790b
Er. Krushn Kathrecha <kathrechakrushn@gmail.com>
2024-04-04 22:38:27 +0530

    error while id download

--- common/models/CandidateIdCard.php
+++ common/models/CandidateIdCard.php
@@ -35,7 +35,8 @@ class CandidateIdCard extends \yii\db\ActiveRecord
     {
         return [
             [['candidate_id'], 'integer'],
-            [['candidate_id'], 'unique'],
+            //[['candidate_id'], 'unique'],
+            ['candidate_id', 'unique', 'comboNotUnique' => 'Candidate Id already exist.', 'targetAttribute' => ['candidate_id', 'deleted']],
             [['candidate_id', 'expiry_date'], 'required'],
             [['created_at', 'updated_at'], 'safe'],
             [['candidate_id'], 'exist', 'skipOnError' => true, 'targetClass' => Candidate::className(), 'targetAttribute' => ['candidate_id' => 'candidate_id']],
