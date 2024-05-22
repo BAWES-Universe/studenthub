c505e812f40bab0f832c2ae4feb43f243f3a66fa
Er. Krushn Kathrecha <10800870+krushn@users.noreply.github.com>
2024-05-22 13:22:06 +0530

    id validation only if exists

--- console/controllers/CronController.php
+++ console/controllers/CronController.php
@@ -55,7 +55,8 @@ class CronController extends \yii\console\Controller {
     public function actionValidateCivilId() {
 
         $query = Candidate::find()
-            ->andWhere(new Expression("DATE(candidate_civil_expiry_date) > DATE(NOW())"));
+            ->andWhere(new Expression("candidate_civil_photo_front IS NOT NULL AND 
+                DATE(candidate_civil_expiry_date) > DATE(NOW())"));
 
         $count = 0;
 
