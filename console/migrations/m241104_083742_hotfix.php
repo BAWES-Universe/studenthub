2a44b6c9de3003d3d28549c427c3697299a198e4
Er. Krushn Kathrecha <10800870+krushn@users.noreply.github.com>
2024-11-04 15:55:44 +0530

    hotfix

--- console/migrations/m241104_083742_hotfix.php
+++ console/migrations/m241104_083742_hotfix.php
@@ -14,9 +14,17 @@ class m241104_083742_hotfix extends Migration
      */
     public function safeUp()
     {
-        $this->addColumn("candidate_working_date", "total_approved", $this->integer(11));
-        $this->addColumn("candidate_working_date", "total_rejected", $this->integer(11));
-        $this->addColumn("candidate_working_date", "total_pending", $this->integer(11));
+        $columnData = $this
+            ->getDb()
+            ->getSchema()
+            ->getTableSchema('candidate_working_date')
+            ->getColumn('total_approved');
+
+        if (!$columnData) {
+            $this->addColumn("candidate_working_date", "total_approved", $this->integer(11));
+            $this->addColumn("candidate_working_date", "total_rejected", $this->integer(11));
+            $this->addColumn("candidate_working_date", "total_pending", $this->integer(11));
+        }
 
         $query = CandidateWorkingDate::find();
           //  ->andWhere(['total_time' => 0]);
