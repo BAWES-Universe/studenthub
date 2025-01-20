0b421c3abe9c334bdab7d7af49008e9762241280
Er. Krushn Kathrecha <10800870+krushn@users.noreply.github.com>
2025-01-20 20:28:33 +0530

    hotfix cors

--- staff/modules/v1/controllers/CandidateIdCardController.php
+++ staff/modules/v1/controllers/CandidateIdCardController.php
@@ -37,13 +37,22 @@ class CandidateIdCardController extends Controller
                 'Origin' => Yii::$app->params['allowedOrigins'],
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                 'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Headers' => ['*'],
                 'Access-Control-Allow-Credentials' => null,
                 'Access-Control-Max-Age' => 86400,
                 'Access-Control-Expose-Headers' => [
                     'X-Pagination-Current-Page',
                     'X-Pagination-Page-Count',
                     'X-Pagination-Per-Page',
-                    'X-Pagination-Total-Count'
+                    'X-Pagination-Total-Count',
+                    'Content-Range',
+                    'Pragma',
+                    'Expires',
+                    'Cache-Control',
+                    'Content-Disposition',
+                    'Content-Type',
+                    'Content-Length',
+                    'Location',
                 ],
             ],
         ];
