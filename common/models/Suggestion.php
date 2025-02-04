7f0e3296827d3d2dae6397cbbbf8b5cfbd72830c
Er. Krushn Kathrecha <10800870+krushn@users.noreply.github.com>
2025-02-04 15:00:04 +0530

    hotfix

--- common/models/Suggestion.php
+++ common/models/Suggestion.php
@@ -136,7 +136,20 @@ class Suggestion extends \yii\db\ActiveRecord
 
         //update `request_updated_at` field
         $this->request->request_updated_datetime = '';
-        $this->request->update(false);
+
+        for ($i = 0; $i < 3; $i++) {
+            try {
+                $this->request->update(false);
+
+                break; // Exit loop if successful
+            } catch (Exception $e) {
+                if ($e->getCode() == 1213) { // Deadlock
+                    sleep(1); // Brief pause before retry
+                    continue;
+                }
+                throw $e; // Rethrow other exceptions
+            }
+        }
 
         if(YII_ENV == 'prod') {
             if ($insert)
