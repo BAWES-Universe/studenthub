b6c5a35e4e366754530c643e085eecc552a3e4ff
Er. Krushn Kathrecha <10800870+krushn@users.noreply.github.com>
2025-03-04 19:07:54 +0530

    part of tech-1147 : wallet staging app

--- environments/dev-server-railway/common/config/main-local.php
+++ environments/dev-server-railway/common/config/main-local.php
@@ -19,7 +19,7 @@ return [
         'walletManager' => [
             'class' => 'common\components\WalletManager',
             'apiKey' => 'QSw2ByGUITXFNjJVNNjyzxdbvYP9rXbG',
-            'apiEndpoint' => 'http://localhost/wallet/webhook/web/v1',//todo:
+            'apiEndpoint' => 'https://webhook.dev.wallet.bawes.net/v1',
             'companyWalletUserID' => 'user_fcac8a5f-52a2-11ed-a68e-d85ed3a264df'
         ],
         //todo: replace with yeaster from sandbox
