489a686ee0d34e796a098c7d8ade7766d779c690
Er. Krushn Kathrecha <10800870+krushn@users.noreply.github.com>
2025-03-04 13:04:41 +0530

    part of tech-1151 : migration issue got fixed

--- console/migrations/m210108_092910_rename_company_contact_to_contact.php
+++ console/migrations/m210108_092910_rename_company_contact_to_contact.php
@@ -27,8 +27,18 @@ class m210108_092910_rename_company_contact_to_contact extends Migration
             $this->addColumn('contact','contact_password_reset_token',$this->string()->unique()->null()->after('contact_auth_key'));
         }
 
-        $this->renameColumn('contact','contact_created_datetime','contact_created_at');
-        $this->renameColumn('contact','contact_updated_datetime','contact_updated_at');
+        $tableData = $this
+            ->getDb()
+            ->getSchema()
+            ->getTableSchema('contact');
+
+        if ($tableData && isset($tableData->columns['contact_created_datetime'])) {
+            $this->renameColumn('contact', 'contact_created_datetime', 'contact_created_at');
+        }
+
+        if ($tableData && isset($tableData->columns['contact_updated_datetime'])) {
+            $this->renameColumn('contact', 'contact_updated_datetime', 'contact_updated_at');
+        }
 
 //         add foreign key for table `company_id`
         if (isset($tableData->foreignKeys['fk-company_contact-CASCADE'])) {
