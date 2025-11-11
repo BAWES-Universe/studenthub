1397a99ad52046331aea8a1ddad8ae8eba8159b9
Khalid Al-Mutawa <khalid@bawes.net>
2025-11-11 18:27:26 +0300

    Fix pathing

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -9,11 +9,11 @@ return [
     'oneSignalCandidateAPPID' => 'fe766231-6156-4537-8037-84e3fe1be5da',
     'oneSignalCandidateAPIKey' => 'YTBkODdlMjctOGQ0Ny00NDgwLTkyMmYtOWQ1NTI5ODlmZjY1',
     'finance_transfer' => 'finance+fake@bawes.net',
-    'candidateAppUrl' => 'https://student.dev.studenthub.co/',
-    'inspectorAppUrl' => 'https://inspector.dev.studenthub.co/',
-    'statusAppUrl' => 'https://status.dev.studenthub.co/',
-    'companyAppUrl' => 'https://employer.dev.studenthub.co/',
-    "managerAppUrl" => 'https://manager.dev.studenthub.co/',
-    'staffAppUrl' => 'https://staff.dev.studenthub.co/',
-    'adminAppUrl' => 'https://admin.dev.studenthub.co/',
+    'candidateAppUrl' => 'http://candidate.studenthub.local/'
+    'inspectorAppUrl' => 'http://inspector.studenthub.local/'
+    'statusAppUrl' => 'http://status.studenthub.local/',
+    'companyAppUrl' => 'http://employer.studenthub.local/',
+    "managerAppUrl" => 'http://manager.studenthub.local/',
+    'staffAppUrl' => 'http://staff.studenthub.local/',
+    'adminAppUrl' => 'http://admin.studenthub.local/',
 ];
