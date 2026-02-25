7b023ff371b8a33dc49a350984a5adf263d120f2
Mishari <17602836+Micru0@users.noreply.github.com>
2026-02-25 04:24:19 +0300

    Rotate AWS keys to use environment variables (public-environment-s3-access)
    Updated AWS temporary access keys to use environment variables for better security.

--- common/config/params.php
+++ common/config/params.php
@@ -15,8 +15,8 @@ return [
     'candidate_photo' => 'https://res.cloudinary.com/studenthub/image/upload/v1596525812/',
     'google_api_key' => 'AIzaSyBSM8o4WSIIRn-sNhn-PvO2s0ovZuLDAaw',
     'mailThreshold' => 500,
-    "aws_temp_access_key_id" => 'AKIAWMITDJRKTSQ4T67K',
-    "aws_temp_secret_access_key" => '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',
+    "aws_temp_access_key_id" => getenv('AWS_TEMP_BUCKET_KEY') ?: '',
+    "aws_temp_secret_access_key" => getenv('AWS_TEMP_BUCKET_SECRET') ?: '',
     "elasticMailIpPool" => "Default",
     'bankInfo' => [ //BAWES Bank Info
         'accountName' => 'BAWES FOR COMPUTER AND OPERATION COMPANY',
